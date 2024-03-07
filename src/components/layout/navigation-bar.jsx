e0d7fd2c6c9f190b48b2a2f4e48957727e686f96
KhimmPhunxx <vakhimim1107@gmail.com>
2024-03-07 15:47:35 +0700

    Push Data: 07/03/2024

--- src/components/layout/navigation-bar.jsx
+++ src/components/layout/navigation-bar.jsx
@@ -12,7 +12,6 @@ function NavigationBar() {
   return (
     <div className=''>
        <div className='max-w-7xl mx-10 md:mx-auto bg-gray-200 flex justify-between rounded'>
-       {/* <NavLink to='' className={({isActive})=> isActive ? 'menubar_active font-[400] text-[#615EAB] text-[16px] relative px-2' : "pop menubar hover:text-[#615EAB] font-[400] text-gray-700 text-[16px] relative px-2 dark:text-white"} >Home</NavLink> */}
             <div className='w-[20%]'>
                 <NavLink className={({isActive})=> isActive ? "flex pop p-2 md:p-4 w-full bg-[#E9AFAF] rounded justify-center text-[5px] md:text-xl font-medium": "flex p-2 md:p-4 rounded justify-center text-[5px] md:text-xl font-medium" } to='/laptop'> <IoMdLaptop className='text-2xl mt-1/2 mr-2 hidden md:block' /> Laptop</NavLink>
             </div>
--- src/pages/contact/ContactPage.jsx
+++ src/pages/contact/ContactPage.jsx
@@ -25,34 +25,36 @@ function ContactPage() {
               <h1 className='text-2xl text-white flex border-b pb-2 font-[400]'> 
                 <CiStopwatch className='mr-4 text-3xl'  />  Hour of Operation
               </h1>
-              <p className='text-xl text-gray-300'>Monday - Saturday : 8 AM - 6 PM Sunday : Close</p>
+              <p className='text-xl text-gray-300'>Monday - Sunday : 8 AM - 6 PM </p>
             </div>
 
             <div className=' space-y-4'> 
               <h1 className='text-2xl text-white flex border-b pb-2 font-[400]'> 
                 <IoMdCall className='mr-4 text-3xl'  />  Hour of Operation
               </h1>
-              <p className='text-xl text-gray-300'>014 417 125 | 097 339 985</p>
+              <p className='text-xl text-gray-300'> 099808088 </p>
             </div>
 
             <div className=' space-y-4'>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
               <h1 className='text-2xl text-white flex border-b pb-2 font-[400]'> 
                 <IoMdMail className='mr-4 text-3xl'  />  Hour of Operation
               </h1>
-              <NavLink to={'mailto:Kimseagoodboy@gmail.com'} className='text-xl text-gray-300'>Kimseagoodboy@gmail.com</NavLink>
+             <div>
+                <NavLink to={'mailto:Kimseagoodboy@gmail.com'} className='text-xl text-gray-300'>Kimseagoodboy@gmail.com</NavLink>
+             </div>
             </div>
             <div className='flex space-x-5'> 
               <NavLink >
-                 <FaTelegram className='text-3xl text-white' />
+                 <FaTelegram className='text-3xl text-gray-300 hover:text-white' />
               </NavLink>
               <NavLink target='_blank' to={'https://www.facebook.com/profile.php?id=61556792725526&mibextid=hu50Ix'}>
-                 <FaFacebook className='text-3xl text-white' />
+                 <FaFacebook className='text-3xl text-gray-300 hover:text-white' />
               </NavLink>
               <NavLink >
-                <FaInstagram className='text-3xl text-white' />
+                <FaInstagram className='text-3xl text-gray-300 hover:text-white' />
               </NavLink>
               <NavLink >
-                <FaTwitter className='text-3xl text-white' />
+                <FaTwitter className='text-3xl text-gray-300 hover:text-white' />
               </NavLink>
             </div>
         </div>
@@ -60,7 +62,7 @@ function ContactPage() {
           <div className='w-full h-full'>
             <h1 className='text-4xl font-bold text-gray-600 border-b border-gray-400 pb-2'>Leave us a Message</h1>
             <div className='mt-10'>
-              <form action="" className='space-y-8'>
+              <form className='space-y-8'>
                 <div className='flex space-x-4'>
                   <input type="text" placeholder='Full Name' className='w-[48%] py-5 p-3 border border-gray-400 rounded-md' />
                   <input type="text" placeholder='Your Email' className='w-[48%] py-5 p-3 border border-gray-400 rounded-md' />
@@ -69,7 +71,7 @@ function ContactPage() {
                   <textarea name="" id="" cols="30" rows="13" placeholder='Message' className='w-full p-2 border border-gray-400 rounded-md'></textarea>
                 </div>
                 <div>
-                  <button className='w-full p-5 bg-blue-600 text-white rounded-md'>Send Message</button>
+                  <button type='submit' className='w-full p-5 bg-blue-600 text-white rounded-md'>Send Message</button>
                 </div>
               </form>
             </div>
